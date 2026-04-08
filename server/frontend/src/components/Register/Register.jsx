96f8f7eb00f4873b08075476acba20d3b97d49e4
rushikesh22310225 <rushikesh.22310225@viit.ac.in>
2026-04-08 12:02:06 +0530

    Update Register.jsx

--- server/frontend/src/components/Register/Register.jsx
+++ server/frontend/src/components/Register/Register.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 
 function Register() {
   return (
-    <div>
+    <form>
       <h2>Register</h2>
+
       <input type="text" placeholder="Username" /><br/>
       <input type="text" placeholder="First Name" /><br/>
       <input type="text" placeholder="Last Name" /><br/>
       <input type="email" placeholder="Email" /><br/>
       <input type="password" placeholder="Password" /><br/>
-      <button>Register</button>
-    </div>
+
+      <button type="submit">Register</button>
+    </form>
   );
 }
 
